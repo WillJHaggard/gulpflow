0138efae9fd77850379385935b0982ea462fcc60
Will Haggard <will@wills-mbp.gateway.pace.com>
2015-08-12 14:13:28 -0400

    Implemented concat task with explanation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Workflow with Git, Gulp, and Browserify
 
-##The Developer Dependencies
+##Dependency Notes
 
 ###gulpjs/gulp-util
 ####src: https://github.com/gulpjs/gulp-util
@@ -19,3 +19,11 @@ Fractal <contact@wearefractal.com> (http://wearefractal.com/)
 I have set up a gulp.task to compile from an array of source files
 if needed.
 
+###gulpjs/gulp-concat
+####src: https://github.com/wearefractal/gulp-concat
+
+This is a js concat plugin for gulp by 
+Fractal <contact@wearefractal.com> (http://wearefractal.com/)
+
+I have set up a gulp.task to compile from an array of js authored source files
+that need to be put together into one script.js file.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
+
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -24,4 +32,18 @@ gutil variable log method. THEN, the second .pipe process puts the compiled JS f
 method into a source--in this case 'components/scripts' with the gulp.dest
 
 Very simple and makes sense
+to run this task onl in the terminal it is: gulp coffee
+*/
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
+
+/* 
+The above creates a gulp task that takes the src of jsSources (the previously declared
+array of what js files have been authored) and concats them together into one
+file and puts the script.js concat file into the destination of builds/development/js
+folder. To run this task only in the terminal it is: gulp js
 */
